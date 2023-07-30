4dc3724bb442a0c0de2c8a827bc2fa885ff8893b
20se020 <tomoyasu.kanemoto@gmail.com>
2023-07-30 11:11:22 +0900

    create Test.java

---
+++ Test.java
@@ -0,0 +1,39 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+//Test Class
+public class Test{
+
+    //this method writes the given data into the specified file
+    //and closes the stream
+    static void writeToFile(String greetings,
+                            String firstName,
+                            String lastName,
+                            FileWriter fileWriter) {
+
+        String customizedGreetings = greetings + "! "+
+                                     firstName + " " +
+                                     lastName;
+        try {
+            fileWriter.write(customizedGreetings + "\n");
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+    }//end writeToFile() method
+
+    //main() method
+    public static void main(String[] args) throws IOException {
+
+        //creates a file in append mode and keeps it open
+        FileWriter fileWriter = new FileWriter("Files/file.txt", true);
+
+        //writeToFile() is called to write data into the file.txt
+        writeToFile("Hi", "Mehvish", "Ashiq", fileWriter);
+        writeToFile("Hello", "Tahir", "Raza", fileWriter);
+
+   }//end main()
+
+}//end Test class
